60c61748ec0ed35d2a7beaefd5106bd5a2978f97
RokHyung Son <fhrgud1264@gmail.com>
2021-05-31 02:00:07 +0900

    Initial Commit

---
+++ ptuBus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ptuBus
+//
+//  Created by rokong.me on 2021/05/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ptuBusTests/ptuBusTests.swift
@@ -0,0 +1,33 @@
+//
+//  ptuBusTests.swift
+//  ptuBusTests
+//
+//  Created by rokong.me on 2021/05/31.
+//
+
+import XCTest
+@testable import ptuBus
+
+class ptuBusTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
